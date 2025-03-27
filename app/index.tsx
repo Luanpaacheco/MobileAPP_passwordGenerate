9bb807a1df5302b165c32489dbeb74907e729a39
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2025-03-27 17:27:53 -0300

    feat: Make buttons functional and implement modal logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
 
 import{useState} from 'react'
-import {View, Text, StyleSheet, Image, Touchable, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image, Touchable, TouchableOpacity, Modal} from 'react-native';
 import React from 'react'
 import Slider from '@react-native-community/slider'
+import { ModalPassword } from '@/src/components/modal/modal';
+
+let charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_"
 
 export default function index(){
 
-  const[size, setSizer] = useState(10)
+  const[size, setSize] = useState(10)
+  const[passwordvalue, setPasswordvalue] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function generatePassword(){
+    let password = ''
+    for(let i =0, n = charset.length; i < size; i++){
+      password+=charset.charAt(Math.floor(Math.random()*n))
+    }
+    setPasswordvalue(password)
+    setModalVisible(true)
+  }
 
   return(
     <View style={styles.container}>
@@ -14,7 +28,7 @@ export default function index(){
       source={require('../src/assets/logo.png')}  
       style={styles.logo}
       />
-      <Text style = {styles.title}>20 caracteres</Text>
+      <Text style = {styles.title}>{size} caracteres</Text>
 
       <View style={styles.area}>
         <Slider
@@ -22,12 +36,18 @@ export default function index(){
         minimumValue={6}
         maximumValue={20}
         minimumTrackTintColor="#000"
-        />
+        value={size}
+        onValueChange={(value) => setSize(Number(value.toFixed(0)))}/>
       </View>
 
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={generatePassword}>
         <Text style={styles.btn_text}>Gerar senha</Text>
+        
       </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <ModalPassword/>
+      </Modal>
     </View>
   )
 }
